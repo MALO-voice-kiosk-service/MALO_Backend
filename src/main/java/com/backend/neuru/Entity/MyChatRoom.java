755f4bc11c134361b8056de1473c8f7ecf55122e
hyeinj <jhi2002@hanyang.ac.kr>
2024-07-29 13:27:58 +0900

    MyChatRoom(채팅방참여 테이블) Entity 완성

--- src/main/java/com/backend/neuru/Entity/MyChatRoom.java
+++ src/main/java/com/backend/neuru/Entity/MyChatRoom.java
@@ -1,8 +1,8 @@
 package com.backend.neuru.Entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
+import java.time.LocalTime;
 
 @Data
 @ToString
@@ -12,5 +12,26 @@ import lombok.*;
 @Entity
 @Table(name="my_chatroom")
 public class MyChatRoom {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "exit_time")
+    private LocalTime exit_time;
+
+    @Column(name = "enter_time")
+    private LocalTime enter_time;
+
+    @Column(name = "is_read")
+    private Boolean is_read;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private UserEntity user;
+
+    @ManyToOne
+    @JoinColumn(name = "chatroom_id", nullable = false)
+    private ChatRoom chatRoom;
+
 
 }
