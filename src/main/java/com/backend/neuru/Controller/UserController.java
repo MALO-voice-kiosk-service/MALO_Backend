94974abb590453cd317989af0d8d87b1279c13e3
hyeinj <jhi2002@hanyang.ac.kr>
2024-07-29 16:34:27 +0900

    user controller 작성 시작 및 security & jwt gradle에 추가

--- src/main/java/com/backend/neuru/Controller/UserController.java
+++ src/main/java/com/backend/neuru/Controller/UserController.java
@@ -1,5 +1,33 @@
 package com.backend.neuru.Controller;
 
+import com.backend.neuru.Service.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@Slf4j
+@RestController
+@RequestMapping(value = "/api/auth")
+@RequiredArgsConstructor
+@Validated
 public class UserController {
+    @Autowired
+    private final UserService userService;
+
+//    // 회원가입 API
+//    @PostMapping(value = "/signup")
+//    public ResponseDTO<?> signup(@Valid @RequestBody AuthDTO authDTO){
+//        return authService.signup(authDTO);
+//    }
 
 }
